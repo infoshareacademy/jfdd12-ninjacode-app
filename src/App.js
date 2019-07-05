374fcb26a121f993b9f9dc7fc99683bf1f10a44b
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-07-05 17:03:29 +0200

    setBalance added in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [balance, setBalance] = useState({
+    saldo: 13000,
+    income: 12000,
+    expenses: 1000,
+  })
   return (
     <div className="App">
+    <h1>SALDO: {balance.saldo}</h1>
+    <div> 
+    <h2>Przychody: {balance.income}</h2>
+    <h2>Wydatki: {balance.expenses}</h2>
+    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
