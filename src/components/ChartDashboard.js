132a8f392fd6beddbbb677868290e80505867e52
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-07-06 21:59:28 +0200

    chart

--- src/components/ChartDashboard.js
+++ src/components/ChartDashboard.js
@@ -1,23 +1,28 @@
-import React, { PureComponent } from 'react';
-import {
-  ResponsiveContainer, PieChart, Pie, Legend,
-} from 'recharts';
+import React, { PureComponent } from "react";
+import { ResponsiveContainer, PieChart, Pie, Legend, Cell} from "recharts";
 
 const data = [
-  { name: 'Przychody', value: 300, Legend: 'star' }, 
-  { name: 'Wydatki', value: 800, Legend: 'star' }
+  { name: "Przychody", value: 400},
+  { name: "Wydatki", value: 800 }
 ];
+const colors = ['#3DCC91', '#FFB366'];
 
+  
 export class ChartDashboard extends PureComponent {
-  static jsfiddleUrl = '//jsfiddle.net/alidingling/6okmehja/';
+  // static jsfiddleUrl = '//jsfiddle.net/alidingling/6okmehja/';
 
   render() {
     return (
-      <div style={{ width: '100%', height: 300 }}>
+      <div style={{ width: "100%", height: 300 }}>
         <ResponsiveContainer>
-          <PieChart>
-            <Pie dataKey="value" nameKey="name" data={data} fill="#8884d8" label />
-            <Legend verticalAlign="top" height={36}/>
+          <PieChart width={730} height={250}>
+            <Pie dataKey="value" data={data} cx="100%" cy="100%" outerRadius={80} label>
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index]} />
+              ))}
+            </Pie>
+
+            <Legend verticalAlign="top" height={36} />
           </PieChart>
         </ResponsiveContainer>
       </div>
