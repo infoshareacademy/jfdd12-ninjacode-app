fbbad403a838861300c0cf82339d53bdc4cd3e82
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-07-09 15:07:02 +0200

    DataPicker added and packages to package.json

--- src/Components/ExpensesForm.js
+++ src/Components/ExpensesForm.js
@@ -4,6 +4,7 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles'
 import Input from '@material-ui/core/Input';
+import { MaterialUIPickers } from "./DatePickerExpenses"
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -72,5 +73,6 @@ export  function Inputs() {
              }}
            />
            </div>
+           <MaterialUIPickers />
            </div>
   );}
