b16d83fa63432605a4a70a46020f4d89fabae614
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-07-18 15:57:49 +0200

    Added icons to revenues and costs buttons

--- src/BottomAppBar.js
+++ src/BottomAppBar.js
@@ -4,25 +4,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import Paper from "@material-ui/core/Paper";
 import Fab from "@material-ui/core/Fab";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import Avatar from "@material-ui/core/Avatar";
-import MenuIcon from "@material-ui/icons/Menu";
 import AddIcon from "@material-ui/icons/Add";
-import SearchIcon from "@material-ui/icons/Search";
-import MoreIcon from "@material-ui/icons/MoreVert";
 import Icon from "@material-ui/core/Icon";
 import { Button, Modal } from "@material-ui/core";
 import { ExpensesForm } from "./components/ExpensesForm";
 import { IncomesForm } from "./components/IncomesForm";
 import { positions } from "@material-ui/system";
+import costs from "./icons/costs.svg";
+import revenues from "./icons/revenues.svg";
+
 
 const useStyles = makeStyles(theme => ({
   text: {
@@ -180,6 +172,7 @@ export default function BottomAppBar(props) {
               onClick={handleOpenExpenses}
             >
               Dodaj wydatki
+              <img src={costs} alt="costs" />
             </Button>
             <Button
               style={{ fontSize: 20, marginLeft: 10 }}
@@ -188,6 +181,7 @@ export default function BottomAppBar(props) {
               onClick={handleOpenIncomes}
             >
               Dodaj przychody
+              <img src={revenues} alt="revenues" />
             </Button>
           </div>
         </Modal>
