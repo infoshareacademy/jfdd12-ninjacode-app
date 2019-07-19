e249a202df2d5d99bec4a6d0a8fd98b0c03b8fa1
Joanna Skrzypinska <jm.skrzypinska@gmail.com>
2019-07-19 15:33:32 +0200

    lf

--- src/components/ViewFilter.js
+++ src/components/ViewFilter.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   formControl: {
     margin: theme.spacing(1),
     minWidth: 300
   },
   selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
+    marginTop: theme.spacing(2)
+  }
 }));
 
 export function NativeSelects() {
-
   const classes = useStyles();
   const [state, setState] = React.useState();
 
   const handleChange = transaction => event => {
     setState({
       ...state,
-      [transaction]: event.target.value,
+      [transaction]: event.target.value
     });
   };
 
@@ -36,23 +34,23 @@ export function NativeSelects() {
       <ul>
         <h1> Filtry </h1>
         <FormControl className={classes.formControl} component="fieldset">
-          <InputLabel htmlFor="transaction-native-simple">Rodzaj transakcji</InputLabel>
+          <InputLabel htmlFor="transaction-native-simple">
+            Rodzaj transakcji
+          </InputLabel>
           <Select
             native
             value={state.transaction}
-            onChange={() => { }}
+            onChange={() => {}}
             inputProps={{
-              name: 'age',
-              id: 'age-native-simple',
+              name: "age",
+              id: "age-native-simple"
             }}
           >
             <option value="" />
             <option value="Przychód">Przychód</option>
             <option value="Wydatej">Wydatek</option>
           </Select>
-
         </FormControl>
-
       </ul>
     </div>
   );
