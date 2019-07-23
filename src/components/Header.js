5e94ec1d4b8617a7c09b8b95a1def82898a768c2
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-07-23 16:16:03 +0200

    Stylized header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,9 @@ import styles from "./Header.module.css";
 
 export function Header({ title }) {
   return (
-    <div className={styles.divA}>
-      <div className={styles.classA}><img src={logo} alt="Logo" className={styles.icon} /></div>
-      <div className={styles.classB}><h2 >{title}</h2></div>
-      {/* <div className={styles.classC}><img src={logo} alt="Logo" className={styles.icon} /></div> */}
+    <div className={styles.headerCont}>
+      <div className={styles.headerIcon}><img src={logo} alt="Logo" className={styles.icon} /></div>
+      <div className={styles.classB}><h2>{title}</h2></div>
     </div>
   );
 }
