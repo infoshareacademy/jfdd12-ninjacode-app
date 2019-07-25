820ad333eb10ca7f98997e88062f3f8f305ce0c6
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-07-25 16:47:07 +0200

    Added type filter to transaction history table

--- src/components/HistoryTable/HistoryTable.js
+++ src/components/HistoryTable/HistoryTable.js
@@ -18,16 +18,24 @@ function filteredTableSum(data) {
   return tableSum.toFixed(2);
 }
 
-function filterData(data, search) {
-  return data.filter(row => {
-    if (search) {
-      return (
-        row.name.toLowerCase().includes(search.toLowerCase()) ||
-        row.category.toLowerCase().includes(search.toLowerCase()) ||
-        String(row.amount).includes(search)
-      );
-    } else return true;
-  });
+function filterData(data, search, type) {
+  return data
+    .filter(row => {
+      if (row.type === type) {
+        return true;
+      } else if (type === "wszystkie") {
+        return true;
+      }
+    })
+    .filter(row => {
+      if (search) {
+        return (
+          row.name.toLowerCase().includes(search.toLowerCase()) ||
+          row.category.toLowerCase().includes(search.toLowerCase()) ||
+          String(row.amount).includes(search)
+        );
+      } else return true;
+    });
 }
 
 const getColumns = data => {
@@ -69,7 +77,8 @@ export class HistoryTable extends React.Component {
     this.state = {
       data: props.data,
       filteredData: props.data,
-      search: ""
+      search: "",
+      selectedFilter: "wszystkie"
     };
   }
 
@@ -77,17 +86,36 @@ export class HistoryTable extends React.Component {
     this.setState({ search: e.target.value });
   };
 
+  onTypeChange = e => {
+    this.setState({ selectedFilter: e.target.value });
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <Layout title={"Historia transakcji"}>
         <BalanceConsumer>
           {({ data }) => {
-            const filteredData = filterData(data, this.state.search);
+            const filteredData = filterData(
+              data,
+              this.state.search,
+              this.state.selectedFilter
+            );
             return (
               <div className={styles.historyContainer}>
                 <div className={styles.historyFind}>
                   Wyszukaj:{" "}
-                  <input value={this.state.search} onChange={this.onSearch} />
+                  <input value={this.state.search} onChange={this.onSearch} />{" "}
+                  Typ:{" "}
+                  <select
+                    name="type"
+                    value={this.state.value}
+                    onChange={this.onTypeChange}
+                  >
+                    <option value="wszystkie">Wszystkie</option>
+                    <option value="wydatki">Wydatki</option>
+                    <option value="wpływy">wpływy</option>
+                  </select>
                 </div>
                 <ReactTable
                   data={filteredData}
